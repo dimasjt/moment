1d69fef24f2d4fe10464fa9a0384f7bc52c0b27c
Dimas J. Taniawan <dimas.taniawan@bocistudio.com>
2017-04-03 15:00:52 +0700

    simple server

--- server/dev.js
+++ server/dev.js
@@ -1,3 +1,24 @@
 import express from 'express'
+import morgan from 'morgan'
+import responseTime from 'response-time'
+import fs from 'fs'
+import path from 'path'
+
+const logFile = path.resolve(__dirname, '../log/dev.log')
+const logDir = path.join(__dirname, 'log')
+const accessLogStream = fs.createWriteStream(logFile, { flags: 'a' })
+
+fs.existsSync(logDir) || fs.mkdirSync(logDir)
 
 const app = express()
+
+app.use(morgan('combined', { stream: accessLogStream }))
+app.use(responseTime())
+
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
+
+app.listen(3000, () => {
+  console.log('Server running')
+})
